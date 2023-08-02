415ebd60ed8ffc44b61a055c1fab0c6d34ddf575
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2023-08-03 00:06:45 +0500

    init

---
+++ first_code.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    string answer = "done";
+    if(answer=="Done")
+    {
+        cout<<"His answer"<<endl;
+    }
+    else{
+        cout<<"her answer"<<endl;
+    }
+    // cout<<"----"<<endl;
+    for (int i=0; i<3;i++)
+    {
+        cout<<" Done"<<endl;
+    }
+    
+    return 0;
+}
